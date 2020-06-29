2dc5c1f160e3ccdf5d22e4f8600d146ed0bf2235
Jeremy Yao <jeremy.yao.info@gmail.com>
2020-06-29 10:00:58 -0400

    Add MVC< alertWindow, and base scene

---
+++ src/MenuInterfaceModel.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Jeremy
+ *
+ */
+public class MenuInterfaceModel
+{
+
+    public MenuInterfaceModel()
+    {
+	// TODO Auto-generated constructor stub
+    }
+
+}
---
+++ src/Scenes/SceneTester.java
@@ -0,0 +1,40 @@
+package Scenes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ * Generic class to load and test scenes. 
+ * 
+ * @author Jeremy
+ *
+ */
+public final class SceneTester extends Application
+{
+
+    public SceneTester()
+    {
+	// TODO Auto-generated constructor stub
+	//Has to be public
+    }
+
+    public static void main(String[] args)
+    {
+	//From application, starts setup and other javafx stuff, and calls start
+	launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+	primaryStage.setTitle("Scene tester");
+	Scene toBeShown = new GenericBackNextScene();
+	
+	primaryStage.setScene(toBeShown);
+	primaryStage.show();
+    }
+
+}
