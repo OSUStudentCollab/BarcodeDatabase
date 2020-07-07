ed839f08a0c49a47c8faf564c6659467eb0fdc1b
Eupho <19samiis@gmail.com>
2020-07-07 16:20:42 -0400

    Moved some methods to a new class

--- src/Utilities/CSVUtilities.java
+++ src/Utilities/CSVUtilities.java
@@ -5,12 +5,15 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static Utilities.Utilities.countCharacters;
+import static Utilities.Utilities.textReader;
+
 /**
  * This is a utility class that helps to read and write to and from a csv file
  *
  * @author Samiul
  */
-public class CSVUtility
+public class CSVUtilities
 {
 	
 	/**
@@ -67,18 +70,6 @@ public class CSVUtility
 	}
 	
 	/**
-	 * Look for the first instance of searchTerm in array
-	 *
-	 * @param array      Array to search
-	 * @param searchTerm Term to look for
-	 * @return Index of searchTerm in array; -1 if searchTerm is not in array
-	 */
-	public static int positionOfElementInArray(String[] array, String searchTerm)
-	{
-		return Arrays.asList(array).indexOf(searchTerm);
-	}
-	
-	/**
 	 * Takes a csv string and returns an array separating the data from the csv string
 	 *
 	 * @param csvText CSV formatted String
@@ -87,14 +78,7 @@ public class CSVUtility
 	 */
 	private static String[] fillArray(String csvText, int size)
 	{
-		String[] array = new String[size];
-		
-		for (int i = 0; i < array.length; i++)
-		{
-			array[i] = readColumnnFromCSVFormat(csvText, i);
-		}
-		
-		return array;
+		return Utilities.fillArray(csvText, size, ',');
 	}
 	
 	/**
@@ -106,85 +90,6 @@ public class CSVUtility
 	 */
 	private static String readColumnnFromCSVFormat(String text, int columnNumber)
 	{
-		StringBuilder element = new StringBuilder();
-		int commasFound = 0;
-		
-		for (int i = 0; i < text.length(); i++)
-		{
-			//Get next character in line
-			char toAppend = text.charAt(i);
-			
-			//Check if comma
-			if (toAppend == ',')
-			{
-				commasFound++;
-				
-				//Check if we should end
-				if (commasFound > columnNumber)
-				{
-					break;
-				}
-				//Erase element if wrong column
-				else
-				{
-					element.delete(0, element.length());
-				}
-			}
-			//Append to element if not comma
-			else
-			{
-				element.append(toAppend);
-			}
-		}
-		
-		return element.toString();
+		return Utilities.readColumnnFromText(text, columnNumber, ',');
 	}
-	
-	/**
-	 * Read text from a file
-	 *
-	 * @param location File location
-	 * @return Text in file
-	 */
-	private static Scanner textReader(String location)
-	{
-		//Make a scanner
-		Scanner scanner = null;
-		
-		//Read file
-		try
-		{
-			scanner = new Scanner(new File(location));
-		} catch (FileNotFoundException e)
-		{
-			System.err.println("File not found");
-		}
-		
-		//Return text
-		return scanner;
-	}
-	
-	/**
-	 * Count instances of characterToCount in text
-	 *
-	 * @param text             String to count search
-	 * @param characterToCount Character to count
-	 * @return Instances of characterToCount in text
-	 */
-	private static int countCharacters(String text, char characterToCount)
-	{
-		int charCount = 0;
-		
-		for (int i = 0; i < text.length(); i++)
-		{
-			if (text.charAt(i) == characterToCount)
-			{
-				charCount++;
-			}
-		}
-		
-		return charCount;
-	}
-	
-	
 }
